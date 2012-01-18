4f12e007e91d0b1cbef741cb1a9f33176de3f9eb
Jeong-Sheng Lim <jslim89@gmail.com>
2012-01-18 21:47:48 +0800

    - re-format the layout of monk

--- application/views/admin/monk/list.php
+++ application/views/admin/monk/list.php
@@ -26,6 +26,10 @@ $(document).ready(function() {
         delete_row_confirmation(delete_urls, row_ids);
     });
 
+    $('#btn_add_new').click(function() {
+        redirect('<?php echo site_url('admin/monk/add'); ?>');
+    });
+
     /*
     $('span#advanced_search').click(function() {
     });
@@ -48,75 +52,85 @@ $(document).ready(function() {
 <!-- End Pagination -->
 
 <!-- Action Button -->
-<div class="grid_6 action-button">
-    <input type="button" class="button" id="btn_delete" 
-        value="<?php echo lang('delete');?>" />
-    <input type="button" class="button" 
-        value="<?php echo lang('add_new');?>" 
-        onclick="redirect('<?php echo site_url('admin/monk/add');?>');" />
-</div>
+<div class="grid_5 right"><?php
+    echo button_link(
+        false,
+        lang('delete'),
+        array('id' => 'btn_delete')
+    );
+    echo nbs(2);
+    echo button_link(
+        false,
+        lang('add_new'),
+        array('id' => 'btn_add_new')
+    );
+?></div>
 <!-- End Action Button -->
 <?php echo clear_div();?>
 
 <div class="grid_16">
-    <table class="listing">
-        <tr>
-            <th width="5%"><?php
-                echo form_checkbox(array(
-                    'name'  => 'check_all',
-                    'id'    => 'check_all',
-                    'value' => 'CHECK_ALL',
-                ));
-            ?></th>
-            <th width="20%"><?php echo lang('image');?></th>
-            <th width="20%"><?php echo lang('monk_name');?></th>
-            <th width="75%"><?php echo lang('monk_story');?></th>
-            <th width="75%"><?php echo lang('edit');?></th>
-        </tr>
-        <?php foreach($monks as $monk):?>
-        <tr id="monk_row_<?php echo $monk->id; ?>">
-            <td><?php
-                echo form_checkbox(array(
-                    'name'  => 'check_'.$monk->id,
-                    'id'    => 'check_'.$monk->id,
-                    'value' => $monk->id,
-                    'class' => 'delete_check',
-                ));
-            ?></td>
-            <td><?php 
-                $image_src = $monk->primary_image_url
-                    ? $monk->primary_image_url
-                    : default_image_path();
-                echo anchor(
-                    site_url('admin/monk/edit/'.$monk->id),
-                    img(array(
-                        'src'    => $image_src,
-                        'alt'    => $monk->monk_name,
-                        'width'  => '100',
-                        'height' => '100',
-                    ))
-                );
-            ?></td>
-            <td><?php 
-                echo anchor(
-                    site_url('admin/monk/edit/'.$monk->id),
-                    $monk->monk_name
-                );
-            ?></td>
-            <td><?php echo $monk->monk_story;?></td>
-            <td>
-                <ul id="icons" class="ui-widget ui-helper-clearfix" style="">
-                    <li class="ui-state-default ui-corner-all">
-                        <span id="edit_<?php echo $monk->id; ?>" class="ui-icon ui-icon-pencil"
-                            title="<?php echo lang('edit');?>"></span>
-                    </li>
-                    <li class="ui-state-default ui-corner-all">
-                        <span id="delete_<?php echo $monk->id; ?>" class="ui-icon ui-icon-trash"
-                            title="<?php echo lang('delete');?>"></span>
-                    </li>
-                </ul>
-            </td>
-        </tr>
-        <?php endforeach;?>
+    <table class="list">
+        <thead>
+            <tr>
+                <td width="1"><?php
+                    echo form_checkbox(array(
+                        'name'  => 'check_all',
+                        'id'    => 'check_all',
+                        'value' => 'CHECK_ALL',
+                    ));
+                ?></td>
+                <td width="15%"><?php echo lang('image');?></td>
+                <td width="20%"><?php echo lang('monk_name');?></td>
+                <td width="75%"><?php echo lang('monk_story');?></td>
+                <td width="5%"><?php echo lang('edit');?></td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($monks as $monk):?>
+            <tr id="monk_row_<?php echo $monk->id; ?>">
+                <td><?php
+                    echo form_checkbox(array(
+                        'name'  => 'check_'.$monk->id,
+                        'id'    => 'check_'.$monk->id,
+                        'value' => $monk->id,
+                        'class' => 'delete_check',
+                    ));
+                ?></td>
+                <td><?php 
+                    $image_src = $monk->primary_image_url
+                        ? $monk->primary_image_url
+                        : default_image_path();
+                    echo anchor(
+                        site_url('admin/monk/edit/'.$monk->id),
+                        img(array(
+                            'src'    => $image_src,
+                            'alt'    => $monk->monk_name,
+                            'width'  => '100',
+                            'height' => '100',
+                        ))
+                    );
+                ?></td>
+                <td><?php 
+                    echo anchor(
+                        site_url('admin/monk/edit/'.$monk->id),
+                        $monk->monk_name
+                    );
+                ?></td>
+                <td><?php echo $monk->monk_story;?></td>
+                <td>
+                    <ul id="icons" class="ui-widget ui-helper-clearfix" style="">
+                        <li class="ui-state-default ui-corner-all">
+                            <span id="edit_<?php echo $monk->id; ?>" class="ui-icon ui-icon-pencil"
+                                title="<?php echo lang('edit');?>"></span>
+                        </li>
+                        <li class="ui-state-default ui-corner-all">
+                            <span id="delete_<?php echo $monk->id; ?>" class="ui-icon ui-icon-trash"
+                                title="<?php echo lang('delete');?>"></span>
+                        </li>
+                    </ul>
+                </td>
+            </tr>
+            <?php endforeach;?>
+        </tbody>
     </table>
 </div>
